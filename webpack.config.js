c932ecc79a52b22fee6c429b01ac01a07707f206
Thiago Oliveira <thiagooliv_@outlook.com>
2021-11-02 21:12:07 -0300

    chore: add and configure sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,20 @@ module.exports = (webpackConfigEnv, argv) => {
   });
 
   return merge(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+      ],
+    },
   });
 };
